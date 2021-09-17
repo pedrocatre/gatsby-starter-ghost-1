b43530664c52cffd22f8484f7f960ad36aa61a5f
pedro <gil.catre@gmail.com>
2021-09-17 21:25:16 +0200

    update URL

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://vibrant-wright-4e27aa.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://optimistic-albattani-9b3eac.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
